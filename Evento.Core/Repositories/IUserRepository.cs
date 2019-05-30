a353c37d826897d6812c34f187f13430d2a74759
Krzysztof Radwanski <krzysztof.radwanski@pl.abb.com>
2019-05-30 13:54:23 +0200

    added RepositoryInterfaces

---
+++ Evento.Core/Repositories/IUserRepository.cs
@@ -0,0 +1,15 @@
+﻿using Evento.Core.Domain;
+using System;
+using System.Threading.Tasks;
+
+namespace Evento.Core.Repositories
+{
+    interface IUserRepository
+    {
+        Task<User> GetAsync(Guid id);
+        Task<User> GetAsync(string email);
+        Task AddAsync(User user);
+        Task UpdateAsync(User user);
+        Task DeleteAsync(User user);
+    }
+}
