2591482bb79c42a3645f5a49a12fa5bb5e9fd12e
Krzysztof Radwanski <krzysztof.radwanski@pl.abb.com>
2019-05-30 15:13:48 +0200

    working on DTO and eventService

---
+++ Evento.Infrastructure/Services/IEventService.cs
@@ -0,0 +1,19 @@
+﻿using Evento.Infrastructure.DTO;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace Evento.Infrastructure.Services
+{
+    public interface IEventService
+    {
+        Task<EventoDTO> GetAsync(Guid id);
+        Task<EventoDTO> GetAsync(string name);
+        Task<IEnumerable<EventoDTO>> BrowseSAync(string name = null);
+        Task CreateAsync(Guid id, string name, string description,
+            DateTime startDate, DateTime endDate);
+        Task AddTicketsAsync(Guid eventId, int amount, decimal price);
+        Task UpdateAsync(Guid id, string name, string description);
+        Task DeleteAsync(Guid id);
+    }
+}
